3c0a18f339fcc6130ca2548c9f2ba810edd4f487
Marvin Drude <marvin@gidd.io>
2026-01-25 13:20:06 +0100

    Fix project langs

--- Me.Memory.Embedded.Generator/SharedSourceGenerator.cs
+++ Me.Memory.Embedded.Generator/SharedSourceGenerator.cs
@@ -8,9 +8,6 @@ public sealed partial class SharedSourceGenerator : IIncrementalGenerator
 {
    public void Initialize(IncrementalGeneratorInitializationContext context)
    {
-      context.RegisterPostInitializationOutput(static (ctx) =>
-      {
-         EmbeddedPayload.Add(ctx);
-      });
+      context.RegisterPostInitializationOutput(EmbeddedPayload.Add);
    }
 }
