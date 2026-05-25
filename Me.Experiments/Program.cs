35f36c1a8510310f0ca880a8734ba249c9a767a3
Marvin Drude <marvindrude1@gmail.com>
2026-05-25 21:54:56 +0200

    Add `Me.Memory` dependency, update `Me.Memory.Serialization.Generator` project configuration, and extend `Program.cs` with `MeSerializer` usage example.

--- Me.Experiments/Program.cs
+++ Me.Experiments/Program.cs
@@ -4,8 +4,14 @@ using Me.Memory.Buffers.Spans;
 using Me.Memory.Collections;
 using Me.Memory.Extensions;
 using Me.Memory.Pools;
+using Me.Memory.Serialization;
 using Me.Memory.Services;
 using Me.Memory.Utils;
 using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("Hello World!");
+
+var bytes = MeSerializer.SerializeWithoutPrecalculation((uint?)200, 256);
+var deserialized = MeSerializer.Deserialize<int>(bytes);
+
+Console.WriteLine(deserialized);
