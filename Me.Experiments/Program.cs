df488c231731a420b67abc812f6158d06589dcec
Marvin Drude <marvindrude1@gmail.com>
2026-05-25 20:55:50 +0200

    Update projects and dependencies: add `Me.Memory.Serialization.Generator` references, align project structure, bump versions to 1.5.0, and remove unused code

--- Me.Experiments/Program.cs
+++ Me.Experiments/Program.cs
@@ -8,25 +8,4 @@ using Me.Memory.Services;
 using Me.Memory.Utils;
 using Microsoft.Extensions.DependencyInjection;
 
-var result = new AsyncTimerResult();
-using (new AsyncTimer(result))
-{
-   await Task.Delay(200);
-}
-
-var time = TimeSpan.Zero;
-using (new StackTimer(ref time))
-{
-   Thread.Sleep(300);
-}
-
-var times = 0L;
-using (new StackTimer(ref times))
-{
-   Thread.Sleep(400);
-}
-
-
-Console.WriteLine("Hello World! " + result.Elapsed.TotalMilliseconds);
-Console.WriteLine("Hello World! " + time.TotalMilliseconds);
-Console.WriteLine("Hello World! " + new TimeSpan(times).TotalMilliseconds);
+Console.WriteLine("Hello World!");
